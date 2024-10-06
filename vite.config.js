05d841e77b29260f53db9c0e2f5637eaebb04f8d
sgcm14 <sgcm14@gmail.com>
2024-10-05 23:11:49 -0500

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/sgcm14/0523C02-router-params-app/", // Ruta del repositorio
+  base: "https://sgcm14.github.io/0523C02-router-params-app/", // Ruta del repositorio
   // base:"/", // para local descomentar aqui y comentar arriba
   plugins: [react()],
 })
