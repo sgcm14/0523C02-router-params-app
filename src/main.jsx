0673aa1e8760b0c8edbb56f01209e01cd31f5e10
sgcm14 <sgcm14@gmail.com>
2024-10-05 23:05:36 -0500

    add deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Faqs from "./Faqs";
@@ -8,9 +8,38 @@ import App from "./App";
 import Faq from "./Faq";
 import "./index.css";
 
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/faqs",
+    element: <Faqs />,
+  },
+  {
+    path: "faqs/:id",
+    element: <Faq />,
+  },
+  {
+    path: "*",
+    element: <h1>No encontrado</h1>,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+     <RouterProvider router={router} />
+     {/* Para deploy se cambia por createHashRouter */}
+    {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<Home />} />
@@ -19,6 +48,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path='faqs/:id' element={<Faq/>} />
         <Route path='*' element={<h1>No encontrado</h1>} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/sgcm14/0523C02-router-params-app/", // Ruta del repositorio
+  // base:"/", // para local descomentar aqui y comentar arriba
   plugins: [react()],
 })
